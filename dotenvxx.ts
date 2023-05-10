dd10e28df553c7a7c4a1bdc78d345f41d3a59bec
Jim Zandueta <jimz@regalcredit.com>
2023-05-11 01:19:39 +0800

    WIP

--- dotenvxx.ts
+++
@@ -1,6 +0,0 @@
-import { config as dotenvConfig } from 'dotenv';
-import { join } from 'path';
-
-dotenvConfig({
-  path: join(__dirname, `../../.env.${process.env.NODE_ENV || 'development'}.${process.env.HOST || 'local'}`),
-});
